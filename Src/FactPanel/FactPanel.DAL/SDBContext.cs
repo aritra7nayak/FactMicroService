9ad78408c464dcd0fcd63bb413f579c5c056eb63
Aritra Nayak <aritra.dhrub@gmail.com>
2023-09-19 08:13:59 +0530

    yo

---
+++ Src/FactPanel/FactPanel.DAL/SDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactPanel.DAL
+{
+    public class SDBContext: DbContext
+    {
+        public SDBContext(): base("DBConnection") { }
+    }
+}
